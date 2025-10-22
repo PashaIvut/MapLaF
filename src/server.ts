30a48be4daaeb4db6b32b99c329d32ae8e94d5b4
Павел Ивуть <ivutjob@yandex.ru>
2025-10-22 19:36:02 +0300

    feat(server): обновить server.ts для загрузки схемы из сгенерированных файлов

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import { createServer } from 'http';
-import {  createYoga } from 'graphql-yoga';
+import {  createYoga, createSchema } from 'graphql-yoga';
 import { config } from './config'
+import { typeDefs } from './schema/typeDefs.generated'
+import { resolvers } from './schema/resolvers.generated'
 
 function createYogaServer() {
-    // const yoga = createYoga({ schema })
-    // const server = createServer(yoga)
-    // server.listen(config.port, () => {
-    //     console.log(`GraphQL сервер запущен на http://localhost:${config.port}/graphql`)
-    // })
+    const yoga = createYoga({ schema: createSchema({ typeDefs, resolvers }) })
+    const server = createServer(yoga)
+    server.listen(config.port, () => {
+        console.log(`GraphQL сервер запущен на http://localhost:${config.port}/graphql`)
+    })
 }
 
 export { createYogaServer } 
