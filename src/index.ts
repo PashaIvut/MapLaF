b4953f4bae82bdddd70978733632ba265844a7a3
Павел Ивуть <ivutjob@yandex.ru>
2025-10-22 17:42:30 +0300

    feat(graphql): добавить Union типы и enum для ошибок в схему

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { createYogaServer } from './server.ts'
-import { connectDb } from './db.ts'
-
-await connectDb()
-createYogaServer()
--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,11 @@ import {  createYoga } from 'graphql-yoga';
 import { config } from './config'
 
 function createYogaServer() {
-    const yoga = createYoga({ schema })
-    const server = createServer(yoga)
-    server.listen(config.port, () => {
-        console.log(`GraphQL сервер запущен на http://localhost:${config.port}/graphql`)
-    })
+    // const yoga = createYoga({ schema })
+    // const server = createServer(yoga)
+    // server.listen(config.port, () => {
+    //     console.log(`GraphQL сервер запущен на http://localhost:${config.port}/graphql`)
+    // })
 }
 
 export { createYogaServer } 
